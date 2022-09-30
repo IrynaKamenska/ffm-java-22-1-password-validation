0d39e149c706f78bdac3d6e4c4ae9f70323b8e47
ToLa09 <landgraf-tobias@web.de>
2022-09-30 11:11:50 +0200

    Add check for spaces and null Add maximal length

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,29 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println(validatePasswordBool(""));
     }
 
-    public static boolean validatePassword(String password) {
-
-        return true;
+    public static String validatePassword(String password) {
+        if (password == null) {
+            return "Passwort ist leer";
+        } else if (password.length() < 8) {
+            return "Passwort zu kurz";
+        } else if (password.contains(" ")) {
+            return "Passwort enthält Leerzeichen";
+        }
+        else
+        return "Passwort korrekt";
+    }
+    public static boolean validatePasswordBool(String password) {
+        if (password == null) {
+            return false;
+        } else if (password.length() < 8 || password.length() > 20) {
+            return false;
+        } else if (password.contains(" ")) {
+            return false;
+        }
+        else
+            return true;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,16 +5,73 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    public void checkLengthEqual8(){
+    public void passwordLengthGreaterThan8(){
         //given
         String password = "12345678";
 
         //when
-        boolean actual = Main.validatePassword(password);
+       String actual = Main.validatePassword(password);
 
         //then
-        assertTrue(actual);
+        String expected = "Passwort korrekt";
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void passwordLengthSmallerThan8(){
+        //given
+        String password = "1234567";
+
+        //when
+        boolean actual = Main.validatePasswordBool(password);
+
+        //then
+        assertFalse(actual);
+    }
+    @Test
+    public void passwordLengthBiggerThan20(){
+        //given
+        String password = "123456790123456789011";
+
+        //when
+        boolean actual = Main.validatePasswordBool(password);
+
+        //then
+        assertFalse(actual);
+    }
+    @Test
+    public void passwordIsNull() {
+        //given
+        String password = null;
+
+        //when
+        boolean actual = Main.validatePasswordBool(password);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void passwordContainsSpace() {
+        //given
+        String password = "huha hahu";
+
+        //when
+        boolean actual = Main.validatePasswordBool(password);
+
+        //then
+        assertFalse(actual);
+    }
+    @Test
+    public void passwordIsEmpty() {
+        //given
+        String password = "";
+
+        //when
+        boolean actual = Main.validatePasswordBool(password);
+
+        //then
+        assertFalse(actual);
+    }
 
 }
